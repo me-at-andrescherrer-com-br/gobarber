717f4842846bed0592da9dc08e0b400b3b4290c3
André Scherrer <andre@computerscience.com.br>
2019-11-17 11:02:04 -0300

    migration do usuário

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
   dialect: 'postgres',
   host: 'localhost',
   username: 'postgres',
-  password: 'docker',
+  password: 'gobarber',
   database: 'gobarber',
   port: 5434,
   define: {
